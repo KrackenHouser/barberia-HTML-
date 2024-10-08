cfdeb51c890f2008b7398889138f209ec566a618
KrackenHouser <168078027+KrackenHouser@users.noreply.github.com>
2024-10-08 20:07:15 +0200

    Update galeria.js

--- javascript/galeria.js
+++ javascript/galeria.js
@@ -4,16 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
         { nombre: "Fade Moderno", imagenUrl: "https://scontent-lax3-1.xx.fbcdn.net/v/t39.30808-6/334763533_892153672036737_12525047881690729_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_ohc=TjurC8KQ7AoQ7kNvgGEq0Sp&_nc_ht=scontent-lax3-1.xx&_nc_gid=ATU6dlOvXkENvobReitPudm&oh=00_AYDMRqf8E8wgr0GCuBG3BHtRmmnIBjqJd6sPyAc7MT58HQ&oe=670B3680", descripcion: "Desvanecido con estilo contemporáneo" },
         { nombre: "Pompadour", imagenUrl: "/Images/pompadour.jpg", descripcion: "Estilo clásico con un toque moderno" },
         { nombre: "Corte Clásico", imagenUrl: "/Images/corte-clasico.jpg", descripcion: "Un corte elegante y tradicional" },
-        { nombre: "Fade Moderno", imagenUrl: "/assets/pruebaimg.jpg", descripcion: "Desvanecido con estilo contemporáneo" },
-        { nombre: "Pompadour", imagenUrl: "/Images/pompadour.jpg", descripcion: "Estilo clásico con un toque moderno" },
-        { nombre: "Corte Clásico", imagenUrl: "/Images/corte-clasico.jpg", descripcion: "Un corte elegante y tradicional" },
-        { nombre: "Fade Moderno", imagenUrl: "/assets/pruebaimg.jpg", descripcion: "Desvanecido con estilo contemporáneo" },
-        { nombre: "Pompadour", imagenUrl: "/Images/pompadour.jpg", descripcion: "Estilo clásico con un toque moderno" },
-        { nombre: "Pompadour", imagenUrl: "https://media.metrolatam.com/2020/06/19/dd7f99a888594485-80edbcf342f8f7f3234842a2c68eea00.jpg", descripcion: "Estilo clásico con un toque moderno" },
-        { nombre: "Pompadour", imagenUrl: "/Images/pompadour.jpg", descripcion: "Estilo clásico con un toque moderno" },
+        { nombre: "Fade Moderno", imagenUrl: "/assets/pruebaimg.jpg", descripcion: "Desvanecido con estilo contemporáneo" }
     ];
 
-    const galeriaContainer = document.getElementById('galeria.html');
+    const galeriaContainer = document.getElementById('galeria'); // CORREGIDO
 
     cortes.forEach(corte => {
         const card = document.createElement('div');
