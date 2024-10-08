fd1f796384434deab2d8cf160972b4e7e5121317
KrackenHouser <168078027+KrackenHouser@users.noreply.github.com>
2024-10-08 18:36:48 +0200

    Update galeria.js

--- javascript/galeria.js
+++ javascript/galeria.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
         { nombre: "Corte Clásico", imagenUrl: "/Images/corte-clasico.jpg", descripcion: "Un corte elegante y tradicional" },
         { nombre: "Fade Moderno", imagenUrl: "/assets/pruebaimg.jpg", descripcion: "Desvanecido con estilo contemporáneo" },
         { nombre: "Pompadour", imagenUrl: "/Images/pompadour.jpg", descripcion: "Estilo clásico con un toque moderno" },
-        { nombre: "Pompadour", imagenUrl: "https://scontent-ord5-1.xx.fbcdn.net/v/t39.30808-6/333609444_229358612824247_727969893750845113_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=127cfc&_nc_ohc=IfaPQMQgKmUQ7kNvgF1tior&_nc_ht=scontent-ord5-1.xx&_nc_gid=APS_A7ybVQc8yHXn-Jqr76b&oh=00_AYCE_jtRzgMe_DFlY30R8Es-piBI3ammQvFuI4qFKiiZkw&oe=6701A8DB", descripcion: "Estilo clásico con un toque moderno" },
+        { nombre: "Pompadour", imagenUrl: "https://media.metrolatam.com/2020/06/19/dd7f99a888594485-80edbcf342f8f7f3234842a2c68eea00.jpg", descripcion: "Estilo clásico con un toque moderno" },
         { nombre: "Pompadour", imagenUrl: "/Images/pompadour.jpg", descripcion: "Estilo clásico con un toque moderno" },
     ];
 
